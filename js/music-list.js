146ba2e3c3b16e89c322b5e10871d88eba32a05f
Antonio Rodrigues <antoniovkuhl@gmail.com>
2023-10-23 17:21:51 -0300

    finalizado

---
+++ js/music-list.js
@@ -0,0 +1,20 @@
+let songs = [
+  {
+    name: "Is This Really Happening",
+    artist: "TrackTribe",
+    img: "assets/media/img-1.jpg",
+    src: "assets/songs/music-1.mp3",
+  },
+  {
+    name: "Night Run Away",
+    artist: "An Jone",
+    img: "assets/media/img-2.jpg",
+    src: "assets/songs/music-2.mp3",
+  },
+  {
+    name: "Timpani Beat",
+    artist: "Nana Kwabena",
+    img: "assets/media/img-3.jpg",
+    src: "assets/songs/music-3.mp3",
+  },
+];
